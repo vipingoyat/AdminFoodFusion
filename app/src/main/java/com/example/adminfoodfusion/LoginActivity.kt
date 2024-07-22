8439481346e0d1150dc33b53bad4efe5188d22bb
vipingoyat <e21cseu0694@bennett.edu.in>
2024-07-22 16:56:44 +0530

    DashBoard Desiging

--- app/src/main/java/com/example/adminfoodfusion/LoginActivity.kt
+++ app/src/main/java/com/example/adminfoodfusion/LoginActivity.kt
@@ -2,23 +2,30 @@ package com.example.adminfoodfusion
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfoodfusion.databinding.ActivityLoginBinding
 
-class SplashScreenActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private val binding:ActivityLoginBinding by lazy{
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,SigninActivity::class.java)
+        setContentView(binding.root)
+        binding.AlreadyHaveAccountText.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
             startActivity(intent)
-            finish()   // Back kerne per vapas yha nhi aayega
-        },2000)
+        }
+
+        val locationList = arrayOf("Jaipur","Ajmer","Hisar","Greater Noida")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        binding.ListOfLocation.setAdapter(adapter)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/adminfoodfusion/SigninActivity.kt
+++ app/src/main/java/com/example/adminfoodfusion/SigninActivity.kt
@@ -1,20 +1,36 @@
 package com.example.adminfoodfusion
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfoodfusion.databinding.ActivitySigninBinding
 
 class SigninActivity : AppCompatActivity() {
+    private val binding: ActivitySigninBinding by lazy{
+        ActivitySigninBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signin)
+
+        setContentView(binding.root)
+        binding.LoginbuttonAdmin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.donthavebutton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
 }
